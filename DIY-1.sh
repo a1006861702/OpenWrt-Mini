860b16b65313cdefa431064d183b4bc3b5fea257
a1006861702 <48322501+a1006861702@users.noreply.github.com>
2025-02-17 19:41:17 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -5,7 +5,7 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # 2-添加 PowerOff 关机插件
-git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
+# git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
 
 # 3-添加 opentomcat 主题
 # git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
@@ -16,3 +16,6 @@ git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app
 # 5-添加 PassWall 插件
 # echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 # echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+
+# 6-添加 lucky 插件
+git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
